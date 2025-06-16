e499a9eeaefad9e07a61c4ff2da251f30166f2e1
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-16 13:05:33 +0530

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(urlencoded({extended:true}))
 const corsOptions={
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://instaclone006.netlify.app'],
     credentials:true
 }
 app.use(cors(corsOptions))
