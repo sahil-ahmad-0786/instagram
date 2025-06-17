3ba3f6583aa1dae095c9043dea0dd53f91a5a8f3
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-17 09:17:48 +0530

    origin

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(urlencoded({extended:true}))
 const corsOptions={
-    origin: ['http://localhost:5173','https://insta4563.netlify.app'],
+    origin: ['http://localhost:5173','https://instaclone78.netlify.app'],
     credentials:true
 }
 app.use(cors(corsOptions))
