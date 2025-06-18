9b4fcb4ed1ea38888a8ed08274ebdaa9eb8da97b
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-18 09:21:59 +0530

    main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,30 +7,16 @@ import { Provider } from 'react-redux'
 import store from './redux/Store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
-import { ErrorBoundary } from 'react-error-boundary'
 
 let persistor = persistStore(store)
-
-// Fallback component
-function ErrorFallback({ error, resetErrorBoundary }) {
-  return (
-    <div role="alert" style={{ padding: '2rem', textAlign: 'center', color: 'red' }}>
-      <h2>Something went wrong</h2>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try Again</button>
-    </div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-          <App />
-          <Toaster />
-        </ErrorBoundary>
+        <App />
+        <Toaster />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
