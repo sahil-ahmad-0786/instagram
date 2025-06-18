ddb831690c9acff0734078cf6d8869be5c5afb31
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-18 15:41:54 +0530

    d

--- frontend/src/hooks/useGetUserProfile .jsx
+++ frontend/src/hooks/useGetUserProfile .jsx
@@ -1,32 +1,24 @@
 import { setUserProfile } from "@/redux/authSlice";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
+
 const useGetUserProfile = (userId) => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const res = await axios.get(`https://instagram-5-u9yd.onrender.com`+`/api/v1/user/${userId}/profile`, {
-          withCredentials: true,
-        });
-
-        if (res.data.success) {
-          console.log(res.data.success);
-          dispatch(setUserProfile(res.data.user));
-        } else {
-          dispatch(setUserProfile(null)); // set to null explicitly on fail
+    const dispatch = useDispatch();
+    // const [userProfile, setUserProfile] = useState(null);
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                const res = await axios.get(`https://instaclone-g9h5.onrender.com/api/v1/user/${userId}/profile`, { withCredentials: true });
+                if (res.data.success) { 
+                    dispatch(setUserProfile(res.data.user));
+                }
+            } catch (error) {
+                console.log(error);
+            }
         }
-      } catch (error) {
-        console.error("Failed to fetch profile:", error);
-        dispatch(setUserProfile(null)); // also set null on catch
-      }
-    };
-
-    fetchUserProfile();
-  }, [userId]);
+        fetchUserProfile();
+    }, [userId]);
 };
-
 export default useGetUserProfile;
