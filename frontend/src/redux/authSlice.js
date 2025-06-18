c290f3bce8ac1f885ae65d3b41844407b1f6a917
Shahil Ahamad <sahilahmad31413@gmail.com>
2025-06-18 16:21:43 +0530

    j

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -11,13 +11,13 @@ const authSlice = createSlice({
   reducers: {
     // actions
     setAuthUser: (state, action) => {
-      state.user = action.payload ?? null;
+      state.user = action.payload ;
     },
     setSuggestedUsers: (state, action) => {
       state.suggestedUsers = action.payload;
     },
     setUserProfile: (state, action) => {
-      state.userProfile = action.payload ?? null;
+      state.userProfile = action.payload ;
     },
     setSelectedUser: (state, action) => {
       state.selectedUser = action.payload;
